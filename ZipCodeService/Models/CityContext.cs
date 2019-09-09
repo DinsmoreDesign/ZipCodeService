a12c9cd492c077f9204aa2f15f5197207cf8302c
Derek Dinsmore <derek.dinsmore@switchthink.com>
2019-09-09 09:47:46 -0700

    Adding models and context

---
+++ ZipCodeService/Models/CityContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ZipCodeService.Models
+{
+    public class CityContext : DbContext
+    {
+        public CityContext(DbContextOptions<CityContext> options) : base(options) { }
+        public DbSet<City> Cities { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Date Source=Database/cities.db");
+        }
+    }
+}
